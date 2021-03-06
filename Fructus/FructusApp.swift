9b822862cf9ec51740c1e43249ceb7981f166cbd
Elias Gabriel <elias.gabrielfn12@gmail.com>
2021-03-05 20:37:37 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Elias Gabriel dos Santos Correa on 05/03/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
